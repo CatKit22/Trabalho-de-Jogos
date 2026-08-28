12dad04e4b40d2dfaecf7f780e491364df354474
Catalina <152108912+CatKit22@users.noreply.github.com>
2026-08-28 12:25:53 -0300

    feat: adicionado nome e ajustado retângulo

--- Trab1/Intro.py
+++ Trab1/Intro.py
@@ -7,11 +7,13 @@ pygame.font.init()
 
 
 font = font = pygame.font.Font(None, 50)
-Nome = "Seu Nome"
-rect =  (260, 100, 175, 35)
+Nome = "Catalina"
+w, h = font.size(Nome)
 
 random.seed(Nome)
-x, y =  random.randint(0, 500), random.randint(0, 400)
+x, y = random.randint(0, 500), random.randint(0, 400)
+
+rect = (x, y, w, h)
 
 print(y)
 
