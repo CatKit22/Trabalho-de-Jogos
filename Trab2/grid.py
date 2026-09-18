cf0d95a5850f74e26c0fa258e6fb7f0ae45ef99d
Catalina <152108912+CatKit22@users.noreply.github.com>
2026-09-18 06:12:11 -0300

    feat: Trabalho 2 - Minesweeper

--- Trab2/grid.py
+++ Trab2/grid.py
@@ -1,9 +1,9 @@
 # Classe base abstrata (Abstract Base Class)
 from abc import ABC, abstractmethod
-
+import pygame
+import random
 
 # objeto herda de classe abstrata
-## nunca pode ser criada, só as filhas
 class obj (ABC):
 
     def __init__(self, x, y, sprites):
@@ -11,7 +11,6 @@ class obj (ABC):
         self.y = y
         #lista
         self.sprites = sprites
-
 
     def draw(self, screen):
         for s in self.sprites:
@@ -24,35 +23,248 @@ class obj (ABC):
 
 class Grid (obj):
 
-    # só avisa a construtora da mãe o que fazer
-    # pode, e deve ser extendido para outras caracteristicas nescessárias
-    def __init__(self, x, y, sprites, grid_size):
-        # bom lugar para criar uma matriz de celulas
+    def __init__(self, x, y, sprites, grid_size, cell_size, mines):
         super().__init__(x, y, sprites)
+
+        self.rows = grid_size[1]
+        self.cols = grid_size[0]
+        self.cell_size = cell_size
+        self.mines = mines
+
+        self.game_over = False
+        self.game_won = False
+        self.revealed = 0
+        self.is_unrevealed = 1
+        self.flags = 0
+
+        self.cells = []
+
+        # matriz de cells
+        for row in range(self.rows):
+            cell_row = []
+            for col in range(self.cols):
+                cell_x = col * cell_size
+                cell_y = y + row * cell_size
+                cell = Cell(cell_x, cell_y, sprites, cell_size, col, row)
+                cell_row.append(cell)
+            self.cells.append(cell_row)
+
+    def get_cell(self, row, col):
+
+        if 0 <= col < self.cols and 0 <= row < self.rows:
+            return self.cells[row][col]
+        return
+
+    def get_cell_at_cursor(self, p):
+
+        x, y = p
+        row = (y - self.y) // self.cell_size
+        col = (x - self.x) // self.cell_size
+        return self.get_cell(row, col)
+
+    # gera as minas aleatoriamente e calcula os numeros
+    # a primeira celula pressionada nao pode ser uma mina
+    def set_mines(self, pressed_cell):
+
+        self.is_unrevealed = 0
+        cell_list = []
+
+        for row in self.cells:
+            for cell in row:
+                if cell != pressed_cell:
+                    cell_list.append(cell)
+            
+        mine_cells = random.sample(cell_list, self.mines)
+
+        for cell in mine_cells:
+            cell.is_mine = True
+
+        for row in self.cells:
+            for cell in row:
+                if not cell.is_mine:
+                    count = 0
+                    for t_cols in [-1, 0, 1]:
+                        for t_rows in [-1, 0, 1]:
+                            if not t_cols and not t_rows:
+                                continue
+                            adj_cell = self.get_cell(cell.row + t_rows, cell.col + t_cols)
+                            if adj_cell and adj_cell.is_mine:
+                                count += 1
+                    cell.adjacent_mines = count
+
+    # toggles flag
+    def toggle_flag(self, cell):
+        mine_count = self.mines - self.flags
+
+        if cell and not cell.is_revealed and not self.is_unrevealed:
+            
+            if cell.is_flag:
+                cell.is_flag = not cell.is_flag
+                self.flags -= 1
+            elif not cell.is_flag and mine_count > 0:
+                cell.is_flag = not cell.is_flag
+                self.flags += 1
+
+    def reveal_grid(self):
+
+        for row in self.cells:
+            for cell in row:
+                if cell.is_mine and not cell.is_flag and self.game_won:
+                    cell.is_flag = True
+                    self.flags = self.mines
+                if cell.is_mine and not cell.is_flag and self.game_over:
+                    cell.is_revealed = True
+                if not cell.is_mine and cell.is_flag:
+                    cell.is_revealed = True
+
+    def reveal_cell(self, cell):
+
+        if cell and not cell.is_flag and not self.game_won and not self.game_over:
+            if not cell.is_revealed:
+                cell.is_revealed = True
+                self.revealed += 1
+
+                if self.revealed == self.rows * self.cols - self.mines:
+                    self.reveal_grid()
+                    self.game_won = True
+
+                if cell.adjacent_mines == 0 and not cell.is_mine:
+                    for t_cols in [-1, 0, 1]:
+                        for t_rows in [-1, 0, 1]:
+                            if not t_cols and not t_rows:
+                                continue
+                            else:
+                                adj_cell = self.get_cell(cell.row + t_rows, cell.col + t_cols)
+                                self.reveal_cell(adj_cell)
+
+            if self.revealed == self.rows * self.cols - self.mines and not cell.is_mine:
+                self.game_won = True
+                self.reveal_grid()
+                return
+            
+            if cell.is_mine:
+                cell.is_exploded = True
+                self.game_over = True
+                self.reveal_grid()
+                return
+
+    def restart(self):
+
+        self.game_over = False
+        self.game_won = False
+        self.revealed = 0
+        self.is_unrevealed = 1
+        self.flags = 0
+
+        for row in self.cells:
+            for cell in row:
+                cell.is_mine = False
+                cell.is_revealed = False
+                cell.is_flag = False
+                cell.is_exploded = False
+                cell.adjacent_mines = 0
+        self.is_unrevealed = 1
 
     def draw(self, screen):
 
-        # chama o desenha já pronto de obj, mas pode ser extendido
-        return super().draw(screen)
+        for row in self.cells:
+            for cell in row:
+                cell.draw(screen)
 
-    def update(self, dt):
-        # não chama o de super, porque ele não implementa, crie seu próprio
-        return 
-
+    def update(self):
+        return
 
 class Cell (obj):
 
     # só avisa a construtora da mãe o que fazer
-    # pode, e deve ser extendido para outras caracteristicas nescessárias
-    def __init__(self, x, y, sprites, grid_size):
-        # bom lugar para definir coisas como o fundo da céula
+    def __init__(self, x, y, sprites, cell_size, col, row):
+
         super().__init__(x, y, sprites)
+
+        self.size = cell_size
+        self.col = col
+        self.row = row
+
+        self.is_mine = False
+        self.is_revealed = False
+        self.is_flag = False
+        self.is_exploded = False
+        self.is_pressed = False
+        self.adjacent_mines = 0
 
     def draw(self, screen):
 
-        # chama o desenha já pronto de obj, mas pode ser extendido
-        return super().draw(screen)
+        if self.is_pressed and not self.is_revealed:
+            screen.blit(self.sprites[0], (self.x, self.y))
+            return
+        
+        if not self.is_revealed and self.is_flag:
+            screen.blit(self.sprites[10], (self.x, self.y))
+            return
 
-    def update(self, dt):
-        # não chama o de super, porque ele não implementa, crie seu próprio
-        return 
+        if not self.is_revealed:
+            screen.blit(self.sprites[9], (self.x, self.y))
+            return
+
+        if self.is_exploded:
+            screen.blit(self.sprites[11], (self.x, self.y))
+            return
+
+        if self.is_mine:
+            screen.blit(self.sprites[13], (self.x, self.y))
+            return
+
+        if not self.is_mine:
+            if self.is_flag:
+                screen.blit(self.sprites[12], (self.x, self.y))
+
+            else:
+                screen.blit(self.sprites[self.adjacent_mines], (self.x, self.y))
+            return
+
+    def update(self):
+        return
+
+class Duck (obj):
+
+    def __init__(self, x, y, sprites, cell_size, grid):
+    
+        super().__init__(x, y, sprites)
+        self.size = cell_size
+        self.is_pressed = False
+        self.grid = grid
+
+    def draw(self, screen):
+
+        size = [self.size, self.size]
+
+        if self.is_pressed:
+            self.sprite = pygame.transform.scale(self.sprites[0], size)
+            self.duck = pygame.transform.scale(self.sprites[15], size)
+            screen.blit(self.sprite, (self.x, self.y))
+            screen.blit(self.duck, (self.x+3, self.y+1))
+            return
+
+        if self.grid.game_over:
+            self.sprite = pygame.transform.scale(self.sprites[9], size)
+            self.duck = pygame.transform.scale(self.sprites[16], size)
+            screen.blit(self.sprite, (self.x, self.y))
+            screen.blit(self.duck, (self.x+2, self.y))
+            return
+
+        if self.grid.game_won:
+            self.sprite = pygame.transform.scale(self.sprites[9], size)
+            self.duck = pygame.transform.scale(self.sprites[17], size)
+            screen.blit(self.sprite, (self.x, self.y))
+            screen.blit(self.duck, (self.x+2, self.y))
+            return
+
+        if self.is_pressed == False:
+            self.sprite = pygame.transform.scale(self.sprites[9], size)
+            self.duck = pygame.transform.scale(self.sprites[14], size)
+            screen.blit(self.sprite, (self.x, self.y))
+            screen.blit(self.duck, (self.x+2, self.y))
+            return
+
+    def update(self):
+        return
--- Trab2/main.py
+++ Trab2/main.py
@@ -1,66 +1,151 @@
 import pygame
-from grid import Grid, Cell
-
+import random
+import time
+from grid import Grid, Cell, Duck
 
 pygame.init()
 pygame.font.init()
 
-WIDTH   =  800; HEIGHT =  600
+# fonte
+font_size = 32
+font = pygame.font.Font(f"Trab2/fonts/born2bsporty-fs.otf", font_size)
+
+# numero de celulas, tamanho, qtd de minas
+grid_size = (32, 16)
+cell_size = 16
+mines = 50
+
+# calcula o tamanho da tela baseado no tamanho do grid e cria a janela
+buffer = 52 # para deixar espaco para o patinho e contadores
+WIDTH = grid_size[0] * cell_size
+HEIGHT = grid_size[1] * cell_size + buffer
 screen = pygame.display.set_mode((WIDTH, HEIGHT))  
+pygame.display.set_caption("Minesweeper")
 
-# caso precise de usar fontes na main, descomente
+# pega os sprites dos arquivos
+sprites = []
+sprite_files = ["0", "1", "2", "3", "4", "5", "6", "7", "8",
+                "unpressed", "flag", "bomb_exploded", "bomb_wrong", "bomb",
+                "duck_base", "duck_quack", "duck_lose", "duck_win"]
 
-#font_size
-#font = pygame.font.Font(None, font_size)
+for sprite_file in sprite_files:
+    try:
+        img = pygame.image.load(f"Trab2/images/{sprite_file}.png").convert_alpha()
+        sprites.append(img)
+    except:
+        sprites.append(None)
 
-# caso precise carregar imagens na main, descomente
+pygame.display.set_icon(sprites[17])
 
-#idle = pygame.image.load("images/duck/duck.png").convert_alpha()
-#step = pygame.image.load("images/duck/step.png").convert_alpha()
-#etc
-
-
-#numero de celulas
-grid_size = (5, 10)
-
-
-# Cria a janela
-WIDTH   =  800; HEIGHT =  600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  
-
-#criar objetos, adicione eles a lista
 objects = []
 
+grid_x = 0 # o grid ocupa a largura inteira
+grid_y = buffer # deixa espaco para os contadores e patinho
+grid = Grid(grid_x, grid_y, sprites, grid_size, cell_size, mines)
+objects.append(grid)
+
+# inicializa o patinho
+duck_size = buffer - 5
+duck = Duck(WIDTH/2 - duck_size/2, grid_y/2 - duck_size/2, sprites, duck_size, grid)
+objects.append(duck)
+
+# contadores do jogo + clock
+clock = pygame.time.Clock()
+mine_count = 0
+game_time = 0
+
 while True: 
+    dt = clock.tick(60) / 1000
+
+    # contador comeca quando uma celula for revelada, comeca em 1
+    if not grid.game_over and not grid.game_won and not grid.is_unrevealed:
+        if game_time == 0:
+            game_time = 1
+        game_time += dt
+
+    # eventos!
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             exit()
 
-        # uso do mouse é obrigatório
+        # faz animacao de botao apertado
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]: # 0 botão esquedo 2, direito
-                pass # faça algo
 
-        #caso queira usar levantar o mouse, descomente
-        #elif event.type == pygame.MOUSEBUTTONUP:
-        #                    exit()
+            cursor_x, cursor_y = pygame.mouse.get_pos()
 
+            # pro patinho
+            if (duck.x <= cursor_x <= duck.x + duck.size and 
+                duck.y <= cursor_y <= duck.y + duck.size):
+                if duck and event.button == 1:
+                    duck.is_pressed = True
 
-        # uso do teclado para controle é obrigatório
+            cell = grid.get_cell_at_cursor([cursor_x, cursor_y])
+
+            # pras celulas do jogo
+            if cell and not grid.game_over and not grid.game_won:
+                if event.button == 1 and not cell.is_flag:
+                    cell.is_pressed = True
+                elif event.button == 3: # as flags sao colocadas no button down
+                    grid.toggle_flag(cell)
+
+        # aperta de fato o botao
+        elif event.type == pygame.MOUSEBUTTONUP:
+
+            cursor_x, cursor_y = pygame.mouse.get_pos()
+
+            # tocar no patinho reseta o jogo
+            if (duck.x <= cursor_x <= duck.x + duck.size and 
+                duck.y <= cursor_y <= duck.y + duck.size and duck):
+                if event.button == 1:
+                    duck.is_pressed = False
+                    grid.restart()
+                    game_time = 0
+            elif duck and duck.is_pressed:
+                duck.is_pressed = False
+
+            cell_at_cursor = grid.get_cell_at_cursor([cursor_x, cursor_y])
+
+            # desaperta botao
+            if cell:
+                cell.is_pressed = False
+
+            # se o grid nao tem nenhuma cell relevada, ele vai gerar as minas, excluindo a cell que foi apertada
+            # se grid tem cells reveladas, revela a apertada normalmente
+            if cell and cell == cell_at_cursor:
+                if event.button == 1:
+                    if grid.is_unrevealed == 1:
+                        grid.set_mines(cell)
+                    grid.reveal_cell(cell)
+
         elif event.type == pygame.KEYDOWN:
-            #inclua outras funcionalidades para outras téclas
+            
             if event.key == pygame.K_ESCAPE:
                 exit()
 
-        #atualiza
-        for obj in objects:
-            obj.update(1)
+            # r reseta o jogo
+            if event.key == pygame.K_r:
+                grid.restart()
+                game_time = 0
 
-        # Desenha
-        screen.fill((30, 30, 30))
+    for obj in objects:
+        obj.update()
 
+    screen.fill((192, 192, 192))
 
-        for obj in objects:
-            obj.draw()
+    for obj in objects: 
+        obj.draw(screen)
 
-        pygame.display.flip()
+    mine_count = mines - grid.flags
+
+    # contador de minas
+    text_mines = font.render(f"MINES: {mine_count}", False, (51,51,51))
+    w, h = text_mines.get_size()
+    screen.blit(text_mines, (WIDTH / 20, buffer/2 - h/2 - 2))
+
+    # contador de segundos
+    text_time = font.render(f"TIME: {int(game_time)}", False, (51,51,51))
+    w, h = text_time.get_size()
+    screen.blit(text_time, (WIDTH - WIDTH / 20 - w,  buffer/2 - h/2 - 2))
+
+    pygame.display.flip()
